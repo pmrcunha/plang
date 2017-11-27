a5782655b1a870bf8252d67b259f3ada6d01e727
Paulo Cunha <paulomrcunha@gmail.com>
2017-11-27 23:45:11 +0100

    add a React HOC that lazy loads the translations

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+import React from 'react';
+
 const plang = {};
 
 const getLocale = () => {
-  const locale = 'en-US';
+  // eslint-disable-next-line
+  const locale = document.cookie.replace(/(?:(?:^|.*;\s*)locale\s*\=\s*([^;]*).*$)|^.*$/, '$1');
   return locale;
 };
 
 const getTranslations = ({
   locale = 'en-US',
-  pathToTranslations = '/localization',
+  pathToTranslations = 'localization',
 }) => new Promise((resolve, reject) => {
   const path = `${pathToTranslations}/${locale}/strings.json`;
-  console.log(path);
   const ajax = new XMLHttpRequest();
   ajax.open('GET', path, true);
   ajax.responseType = 'json';
@@ -23,16 +25,33 @@ const getTranslations = ({
   ajax.send();
 });
 
-const setTranslations = (translations) => {
-  console.log(translations);
-  plang.translations = translations;
-};
+const withTranslations = (WrappedComponent, _locale) => class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      translations: {},
+    };
+    this.getTrans();
+  }
 
-const t = key => plang.translations[key]; // eslint-disable-line no-undef
+  getTrans() {
+    getTranslations({ locale: _locale }).then((t) => {
+      this.setState({
+        translations: t,
+      });
+    });
+  }
+
+  render() {
+    return React.createElement(
+      WrappedComponent,
+      Object.assign({}, this.props, { t: this.state.translations }),
+      null,
+    );
+  }
+};
 
 export default Object.assign(plang, {
   getLocale,
-  getTranslations,
-  setTranslations,
-  t,
+  withTranslations,
 });
