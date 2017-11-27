604a1f623f32f6c08d5e444be287c508d0ec3ddb
Paulo Cunha <paulomrcunha@gmail.com>
2017-11-28 00:29:40 +0100

    remove old code and test that don't work

--- index.test.js
+++
@@ -1,9 +0,0 @@
-import { t } from './index';
-
-global.translations = {
-  'path.to.a.string': 'Path to a string'
-}
-
-it('returns a translated string', () => {
-  expect(t('path.to.a.string')).toBe('Path to a string');
-});
